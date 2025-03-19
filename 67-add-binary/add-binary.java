01ad693cc5173a7d3b59dc45729bfc16716b106b
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2025-03-19 13:32:09 +0530

    Time: 1 ms (99.66%) | Memory: 42.4 MB (52.87%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,36 +1,33 @@
 class Solution {
-    public String addBinary(String a, String b) {
-       int i=a.length()-1;int j=b.length()-1;
-       int carry=0;
-       String str="";
-       while(i>=0&&j>=0)
-       {
-        int ta=a.charAt(i)=='0'?0:1;
-        int tb=b.charAt(j)=='0'?0:1;
-        int temp=(ta^tb)^carry;
-        if(((ta==1)&&(tb==1))||((tb==1)&&(carry==1))||((carry==1)&&(ta==1))){carry=1;}
-        else{carry=0;}
-           str=temp+str;--i;--j;
-       }
-       while(i>=0)
-       {
-            int ta=a.charAt(i)=='0'?0:1;
-            int temp=ta^carry;
-            if((ta==1)&&(carry==1)){carry=1;}
-            else{carry=0;}
-            str=temp+str;
-            --i;
-       }
-       while(j>=0)
-       {
-            int tb=b.charAt(j)=='0'?0:1;
-            int temp=tb^carry;
-            if((tb==1)&&(carry==1)){carry=1;}
-            else{carry=0;}
-            str=temp+str;
-            --j;
-       }
-       if(carry==1){str=1+str;}
-        return str;
+    public String addBinary(String a, String b) 
+    {
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while(i >= 0 || j >= 0 )
+        {
+            int sum = carry;
+            if(i >= 0)
+            {
+                sum += a.charAt(i) - '0';
+            }
+            if(j >= 0)
+            {
+                sum += b.charAt(j) - '0';
+            }
+            sb.append(sum % 2);
+            carry = sum / 2;
+
+            i--;
+            j--;
+        }
+        if(carry > 0)
+        {
+            sb.append(carry);
+        }
+        return sb.reverse().toString();
+
     }
 }
