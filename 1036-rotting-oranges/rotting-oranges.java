0d94ab558b86f6206a40433ba77c171fe744d3b5
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-03-05 14:23:37 +0530

    Time: 2 ms (91.01%) | Memory: 44.7 MB (8.71%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,45 +1,58 @@
-class Solution {
-    int result[][];
-    public void helper(int row,int col,int [][]grid)
+class pair{
+    int r;
+    int c;
+    pair(int r, int c)
     {
-        int r[]={0,1,-1,0};
-        int c[]={1,0,0,-1};
-        for(int i=0;i<4;++i)
-        {
-            int tr=row+r[i];
-            int tc=col+c[i];
-            
-            if(tr>=0&&tr<grid.length&&tc>=0&&tc<grid[row].length&&grid[tr][tc]==1&&result[tr][tc]>result[row][col]+1)
-            {
-                result[tr][tc]=result[row][col]+1;
-                helper(tr,tc,grid);
-            }
-        }
+        this.r=r;
+        this.c=c;
     }
+}
+class Solution {
     public int orangesRotting(int[][] grid) {
-        result=new int[grid.length][grid[0].length];
+        Queue<pair>q=new LinkedList<>();
+        int[][]mat=new int[grid.length][grid[0].length];
         for(int i=0;i<grid.length;++i)
         {
-            Arrays.fill(result[i], Integer.MAX_VALUE);
-        } 
-        for(int i=0;i<grid.length;++i)
-        {
+            Arrays.fill(mat[i],Integer.MAX_VALUE);
             for(int j=0;j<grid[i].length;++j)
             {
                 if(grid[i][j]==2)
                 {
-                    result[i][j]=0;
-                    helper(i,j,grid);
+                    q.add(new pair(i,j));
+                    mat[i][j]=0;
                 }
             }
         }
         int res=0;
+        while(!q.isEmpty())
+        {
+            pair p=q.poll();
+            int r=p.r;
+            int c=p.c;
+            int row[]={0,1,-1,0};
+            int col[]={1,0,0,-1};
+            for(int i=0;i<4;++i)
+            {
+                int tr=r+row[i];
+                int tc=c+col[i];
+                if(tr>=0&&tr<grid.length&&tc>=0&&tc<grid[0].length&&grid[tr][tc]==1&&mat[tr][tc]>mat[r][c]+1)
+                {
+                    mat[tr][tc]=mat[r][c]+1;
+                    grid[tr][tc]=2;
+                    res=Math.max(res,mat[tr][tc]);
+                    q.add(new pair(tr,tc));
+                }
+            }
+        }
         for(int i=0;i<grid.length;++i)
         {
+            Arrays.fill(mat[i],Integer.MAX_VALUE);
             for(int j=0;j<grid[i].length;++j)
             {
-                if(grid[i][j]==1 && result[i][j]==Integer.MAX_VALUE){return -1;}
-                else if(grid[i][j]==1){res=Math.max(res,result[i][j]);}
+                if(grid[i][j]==1)
+                {
+                    return -1;
+                }
             }
         }
         return res;
