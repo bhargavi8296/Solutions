42c5f95ebcd547b82074b695a973b5540289e973
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-03-05 14:32:29 +0530

    Time: 1 ms (45.64%) | Memory: 46.3 MB (99.39%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,25 +1,24 @@
 class Solution {
-    public void helper(int[][]image,int sr,int sc, int color,int co)
+    public void helper(int[][] image, int sr, int sc, int color,int old)
     {
-        int r[]={0,1,0,-1};
-        int c[]={1,0,-1,0};
+        int r[]={1,0,-1,0};
+        int c[]={0,1,0,-1};
         for(int i=0;i<4;++i)
         {
-            int tr=r[i]+sr; int tc=c[i]+sc;
-            if(tr>=0&&tr<image.length&&tc>=0&&tc<image[sr].length&&image[tr][tc]==co)
+            int tr=sr+r[i];int tc=sc+c[i];
+            if(tr>=0&&tr<image.length&&tc>=0&&tc<image[0].length&&image[tr][tc]==old)
             {
                 image[tr][tc]=color;
-                helper(image,tr,tc,color,co);
+                helper(image,tr,tc,color,old);
             }
         }
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int c=image[sr][sc];
-        if(color!=c)
-        {
-            image[sr][sc]=color;
-            helper(image,sr,sc,color,c);
-        }
+        //System.out.print(image[sr][sc]+" "+color);
+        if(image[sr][sc]==color){return image;}
+        int old=image[sr][sc];
+        image[sr][sc]=color;
+        helper(image,sr,sc,color,old);
         return image;
     }
 }
