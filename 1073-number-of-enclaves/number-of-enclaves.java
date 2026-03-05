524a9e73673fdd297e30f0ed849dfd46d7b6a091
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-03-06 04:26:18 +0530

    Time: 9 ms (51.86%) | Memory: 58.3 MB (66.78%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,41 +1,38 @@
 class Solution {
-    int rl;
-    int cl;
-    public void helper(int[][]grid, int r,int c)
+    public void helper(int[][]grid,int r,int c)
     {
+        if(grid[r][c]==0){return;}
         grid[r][c]=0;
-        int rm[]={0,1,0,-1};
-        int cm[]={1,0,-1,0};
+        int ro[]={0,1,0,-1};
+        int co[]={1,0,-1,0};
         for(int i=0;i<4;++i)
         {
-            int tr=r+rm[i];
-            int tc=c+cm[i];
-            if(tr>=0&&tc>=0&&tr<rl&&tc<cl&&grid[tr][tc]==1)
+            int tr=r+ro[i];
+            int tc=c+co[i];
+            if(tr>=0&&tr<grid.length&&tc>=0&&tc<grid[0].length&&grid[tr][tc]==1)
             {
                 helper(grid,tr,tc);
             }
         }
     }
     public int numEnclaves(int[][] grid) {
-        rl=grid.length;
-        cl=grid[0].length;
+        for(int i=0;i<grid.length;++i)
+        {
+            for(int j=0;j<grid[i].length;++j)
+            {
+                if((i==0||j==0||i==grid.length-1||j==grid[0].length-1)&&grid[i][j]==1)
+                {
+                    helper(grid,i,j);
+                }
+            }
+        }
         int count=0;
-        for(int i=0;i<rl;++i)
+        for(int i=0;i<grid.length;++i)
         {
-            if(grid[i][0]==1){helper(grid,i,0);}
-            if(grid[i][cl-1]==1){helper(grid,i,cl-1);}
-        }
-        for(int i=0;i<cl;++i)
-        {
-            if(grid[0][i]==1){helper(grid,0,i);}
-            if(grid[rl-1][i]==1){helper(grid,rl-1,i);}
-        }
-        for(int i=0;i<rl;++i)
-        {
-            for(int j=0;j<cl;++j)
+            for(int j=0;j<grid[i].length;++j)
             {
                 if(grid[i][j]==1)
-                { 
+                {
                     ++count;
                 }
             }
