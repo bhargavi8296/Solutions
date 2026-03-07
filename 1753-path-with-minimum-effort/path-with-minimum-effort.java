843ec5a918b99ae3283a372a27dacb4020858448
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-03-07 16:38:30 +0530

    Time: 101 ms (8.26%) | Memory: 47.9 MB (56.77%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -1,12 +1,3 @@
-class pair{
-    int r;
-    int c;
-    pair(int r,int c)
-    {
-        this.r=r;
-        this.c=c;
-    }
-}
 class Solution {
     public int minimumEffortPath(int[][] heights) {
         int dist[][]=new int[heights.length][heights[0].length];
@@ -14,14 +5,14 @@ class Solution {
         {
             Arrays.fill(dist[i],Integer.MAX_VALUE);
         }
-        Queue<pair>q=new LinkedList<>();
-        q.add(new pair(0,0));
+        Queue<int[]>q=new LinkedList<>();
+        q.add(new int[]{0,0});
         dist[0][0]=0;
         while(!q.isEmpty())
         {
-            pair p=q.poll();
-            int r=p.r;
-            int c=p.c;
+            int p[]=q.poll();
+            int r=p[0]; 
+            int c=p[1];
             int ro[]={0,1,0,-1};
             int co[]={1,0,-1,0};
             for(int i=0;i<4;++i)
@@ -31,7 +22,7 @@ class Solution {
                 if(tr>=0&&tr<heights.length&&tc>=0&&tc<heights[0].length&&dist[tr][tc]>Math.max(dist[r][c],Math.abs(heights[tr][tc]-heights[r][c])))
                 {
                     dist[tr][tc]=Math.max(dist[r][c],Math.abs(heights[tr][tc]-heights[r][c]));
-                    q.add(new pair(tr,tc));
+                    q.add(new int[]{tr,tc});
                 }
             }
         }
