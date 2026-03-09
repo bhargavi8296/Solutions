78b26916c65bbc8d7f127e3c27e5ebaaef2256e5
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-03-09 10:49:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
@@ -1,48 +1,55 @@
 class Solution {
+    int par[];
+    int rank[];
+    public int find(int u)
+    {
+        if(u==par[u]){return u;}
+        return u=find(par[u]);
+    }
+    public void union(int x, int y,int px, int py)
+    {
+        if(rank[x]>rank[y])
+        {
+            par[py]=px;
+            rank[px]+=rank[py];
+        }
+        else{
+            par[px]=py;
+            rank[py]+=rank[px];
+        }
+    }
     public int spanningTree(int V, int[][] edges) {
         // code here
-        ArrayList<ArrayList<int[]>>list=new ArrayList<>();
-        for(int i=0;i<V;++i){list.add(new ArrayList<>());}
-        for(int i=0;i<edges.length;++i)
-        {
-            int f=edges[i][0];int s=edges[i][1]; int d=edges[i][2];
-            list.get(f).add(new int[]{s,d});
-            list.get(s).add(new int[]{f,d});
-        }
-        int vis[]=new int[V];
-        Queue<int[]>q=new PriorityQueue<>((a,b)->a[1]-b[1]);
-        int res=0;
+        par=new int[V];
+        rank=new int[V];
+        Arrays.fill(rank,1);
         for(int i=0;i<V;++i)
         {
-            
-            if(vis[i]==0)
+            par[i]=i;
+        }
+        Queue<int[]>q=new PriorityQueue<>((a,b)->a[2]-b[2]);
+        for(int i=0;i<edges.length;++i)
+        {
+            int f=edges[i][0];
+            int s=edges[i][1];
+            int d=edges[i][2];
+            q.add(new int[]{f,s,d});
+        }
+        int res=0;
+        while(!q.isEmpty())
+        {
+            int []p=q.poll();
+            int f=p[0];
+            int s=p[1];
+            int d=p[2];
+            int px=find(f);
+            int py=find(s);
+            if(px!=py)
             {
-                q.add(new int[]{i,0});
-                while(!q.isEmpty())
-                {
-                    int []p=q.poll();
-                    int node=p[0];
-                    int d=p[1];
-                    if(vis[node]==0)
-                    {
-                        //System.out.println(node+" "+d);
-                        vis[node]=1;
-                        res+=d;
-                    }
-                    else{continue;}
-                    for(int []t:list.get(node))
-                    {
-                        int f=t[0];
-                        int dis=t[1];
-                        if(vis[f]==0)
-                        {
-                            q.add(new int[]{f,dis});
-                        }
-                    }
-                }
+                res+=d;
+                union(f,s,px,py);
             }
         }
         return res;
-        
     }
 }
