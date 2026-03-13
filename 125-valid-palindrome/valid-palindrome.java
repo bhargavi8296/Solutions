50315dec69a5e693e28d497cdf0d78c8c20375c2
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-03-13 17:27:02 +0530

    Time: 2 ms (99.14%) | Memory: 44.9 MB (45.36%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.toLowerCase();
+        int i=0;int j=s.length()-1;
+        while(i<j)
+        {
+            while(i<s.length()&&!(s.charAt(i)>='a'&&s.charAt(i)<='z')&&!(s.charAt(i)>='0'&&s.charAt(i)<='9')){++i;}
+            while(j>=0&&!(s.charAt(j)>='a'&&s.charAt(j)<='z')&&!(s.charAt(j)>='0'&&s.charAt(j)<='9')){--j;}
+            if(i<s.length()&&j>=0)
+            {if(s.charAt(i)!=s.charAt(j)){return false;}
+            ++i;--j;}
+        }
+        return true;
+    }
+}
