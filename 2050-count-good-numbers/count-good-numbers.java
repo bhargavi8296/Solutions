b39fdbc694578bbc8c439e8006b5f6bcb2bfd560
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-03-29 17:37:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (27.69%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.java
+++ 2050-count-good-numbers/count-good-numbers.java
@@ -2,6 +2,7 @@ class Solution {
     static final int MOD = 1000000007;
 
     public int countGoodNumbers(long chakraLength) {
+        if(chakraLength==1){return 5;}
         long evenPositions = (chakraLength + 1) / 2; // like left side of scroll
         long oddPositions = chakraLength / 2;        // right side of scroll
         long evenWays = fastPower(5, evenPositions); // 5 even digits
@@ -12,16 +13,11 @@ class Solution {
 
     // Chakra Power Boost: Fast Exponentiation
     long fastPower(long base, long power) {
-        long result = 1;
-        base %= MOD;
-
-        while (power > 0) {
-            if (power % 2 == 1) {
-                result = (result * base) % MOD; // apply current jutsu
-            }
-            base = (base * base) % MOD; // double chakra burst
-            power /= 2; // halve the turns
+        if(power==0){return 1;}
+        if(power%2==0)
+        {
+            return fastPower((base*base)%MOD,power/2);
         }
-        return result;
+        return (base*fastPower((base*base)%MOD,power/2))%MOD;
     }
 }
