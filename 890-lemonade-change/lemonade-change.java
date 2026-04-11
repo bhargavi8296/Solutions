591ca5f3a6fc2a253ec583ab3a848d6e9e6bb21a
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-04-11 12:37:17 +0530

    Time: 3 ms (11.62%) | Memory: 72.4 MB (74.94%) - LeetSync

--- 890-lemonade-change/lemonade-change.java
+++ 890-lemonade-change/lemonade-change.java
@@ -1,24 +1,24 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
     //   Arrays.sort(bills);
-       Map<Integer,Integer>map=new HashMap<>();
+       int arr[]=new int[5];
        for(int i=0;i<bills.length;++i)
        {
             if(bills[i]!=5){
                 int temp=bills[i]-5;
                 if(temp==15)
                 {
-                    if(map.containsKey(10)&&map.containsKey(5)&&map.get(10)>=1&&map.get(5)>=1){map.put(10,map.get(10)-1);map.put(5,map.get(5)-1);}
-                    else if(map.containsKey(5)&&map.get(5)>=3){map.put(5,map.get(5)-3);}
+                    if(arr[2]>=1&&arr[1]>=1){arr[2]-=1;arr[1]-=1;}
+                    else if(arr[1]>=3){arr[1]-=3;}
                     else{return false;}
                 }
                 else if(temp==5)
                 {
-                    if(map.containsKey(5)&&map.get(5)>=1){map.put(5,map.get(5)-1);}
+                    if(arr[1]>=1){arr[1]-=1;}
                     else{return false;}
                 }
             }
-            map.put(bills[i],map.getOrDefault(bills[i],0)+1);
+            arr[bills[i]/5]++;
        } 
        return true;
     }
