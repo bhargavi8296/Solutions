e670a6cef6244aab801d656561d97579f953d62c
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-05-05 12:30:18 +0530

    Time: 1 ms (100.00%) | Memory: 48.9 MB (32.54%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,36 +1,31 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        boolean flag=(nums1.length+nums2.length)%2==0?true:false;
-        int n=((nums1.length+nums2.length)/2)+1;
-        int count=0;
-        double ele=-1;
-        double ele1=-1;
-        int i=0;int j=0;
-        //System.out.print(n);
-        while(i<nums1.length&&j<nums2.length)
+        if(nums1.length>nums2.length)
         {
-            if(count<n)
-            {
-                if(nums1[i]<=nums2[j])
-                {
-                    ele1=ele;ele=nums1[i];++i;
-                }
-                else{
-                    ele1=ele;ele=nums2[j];++j;
-                }
-            }
-            else{break;}++count;
+            int temp[]=nums1;
+            nums1=nums2;
+            nums2=temp;            
         }
-        //System.out.print(ele+" "+ele1+" "+count+" "+n+" "+i+" "+j);
-        while(count<n&&i<nums1.length)
+        int n1=nums1.length;
+        int n2=nums2.length;
+        int low=0;int high=n1;
+        while(low<=high)
         {
-            ele1=ele;ele=nums1[i];++i;++count;
+            //int mid=(low+high)/2;
+            int r1=(low+high)/2;
+            int r2=((n1+n2+1)/2)-r1;
+            int l1=r1-1;
+            int l2=r2-1;
+            int l1n=l1<0?Integer.MIN_VALUE:nums1[l1];
+            int l2n=l2<0?Integer.MIN_VALUE:nums2[l2];
+            int r1n=r1>=n1?Integer.MAX_VALUE:nums1[r1];
+            int r2n=r2>=n2?Integer.MAX_VALUE:nums2[r2];
+            if(l1n<=r2n&&l2n<=r1n){
+                if((n1+n2)%2==0){return (Math.max(l1n,l2n)+Math.min(r1n,r2n))/2.0;}
+                return Math.max(l1n,l2n);}
+            else if(l1n>r2n){high=r1-1;}
+            else{low=r1+1;}
         }
-        while(count<n&&j<nums2.length)
-        {
-            ele1=ele;ele=nums2[j];++j;++count;
-        }
-        if(flag){return (ele+ele1)/2;}
-        return ele;
+        return 0.0;
     }
 }
