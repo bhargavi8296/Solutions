ab087c6af957919d1d84d04c2c8b8eb7eaf348ca
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-05-14 16:22:45 +0530

    Time: 10 ms (21.99%) | Memory: 49 MB (56.43%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,34 +1,35 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> {
-    if (a[0] != b[0]) {
-        return a[0] - b[0];   // first element
-    } else {
-        return a[1] - b[1];   // second element
-    }});
-        List<List<Integer>>result=new ArrayList<>();
-        int i=0;
-        while(i<intervals.length)
-        {
-            int j=i+1;
-            int second=intervals[i][1];
-            while(j<intervals.length&&second>=intervals[j][0])
+            if (a[0] != b[0]) {
+                return a[0] - b[0];   
+            } else {
+                return a[1] - b[1];   
+            }});
+            List<List<Integer>>list=new ArrayList<>();
+            int i=0;
+            while(i<intervals.length)
             {
-                second=Math.max(second,intervals[j][1]);++j;
+                int last=intervals[i][1];
+                int j=i+1;
+                while(j<intervals.length&&last>=intervals[j][0])
+                {
+                    last=Math.max(last,intervals[j][1]);
+                    ++j;
+                }
+                List<Integer>temp=new ArrayList<>();
+                temp.add(intervals[i][0]);
+                temp.add(last);
+                list.add(temp);
+                i=j;
             }
-            result.add(new ArrayList<>());
-            result.get(result.size()-1).add(intervals[i][0]);
-            result.get(result.size()-1).add(second);
-            i=j;
-        }
-        int res[][]=new int[result.size()][2];
-        int j=0;
-        for(List<Integer>temp:result)
-        {
-            res[j][0]=temp.get(0);
-            res[j][1]=temp.get(1);
-            ++j;
-        }
-        return res;
+            int result[][]=new int[list.size()][2];
+            i=0;
+            for(List<Integer>temp:list)
+            {
+                result[i][0]=temp.get(0);
+                result[i][1]=temp.get(1);++i;
+            }
+            return result;
     }
 }
