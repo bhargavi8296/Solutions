3b90cf8b16bd90d03e43ee473c2b58f04668f587
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-05-22 08:28:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.java
+++ Difficulty: Medium/Topological Sort/topological-sort.java
@@ -1,29 +1,42 @@
 class Solution {
     public ArrayList<Integer> topoSort(int V, int[][] edges) {
         // code here
-        ArrayList<ArrayList<Integer>>list=new ArrayList<>();
+        ArrayList<Integer>result=new ArrayList<>();
         int in[]=new int[V];
-        for(int i=0;i<V;++i){list.add(new ArrayList<>());}
+        ArrayList<ArrayList<Integer>>list=new ArrayList<>();
+        for(int i=0;i<V;++i)
+        {
+            list.add(new ArrayList<>());
+        }
         for(int i=0;i<edges.length;++i)
         {
             int f=edges[i][0];
             int s=edges[i][1];
-            list.get(f).add(s);
             in[s]++;
+            list.get(f).add(s);
         }
         Queue<Integer>q=new LinkedList<>();
-        ArrayList<Integer>res=new ArrayList<>();
-        for(int i=0;i<V;++i){if(in[i]==0){q.add(i);}}
-        while(!q.isEmpty())
+        for(int i=0;i<V;++i)
         {
-            int temp=q.poll();
-            res.add(temp);
-            for(int val:list.get(temp))
+            if(in[i]==0)
             {
-                in[val]--;
-                if(in[val]==0){q.add(val);}
+                q.add(i);
+                result.add(i);
             }
         }
-        return res;
+        while(!q.isEmpty())
+        {
+            int node=q.poll();
+            for(int val:list.get(node))
+            {
+                in[val]--;
+                if(in[val]==0)
+                {
+                    q.add(val);
+                    result.add(val);
+                }
+            }
+        }
+        return result;
     }
 }
