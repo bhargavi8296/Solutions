4c79290709260a3c431370c7b170a446155ffe8f
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-06-13 19:45:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (36.47%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,11 +1,13 @@
 class Solution {
     public int rob(int[] nums) {
         if(nums.length==1){return nums[0];}
-        nums[nums.length-2] = Math.max(nums[nums.length-2], nums[nums.length-1]);
+        int dp[]=new int[nums.length];
+        dp[dp.length-1]=nums[nums.length-1];
+        dp[dp.length-2]=Math.max(nums[nums.length-1],nums[nums.length-2]);
         for(int i=nums.length-3;i>=0;--i)
         {
-            nums[i]=Math.max(nums[i]+nums[i+2],nums[i+1]);
+            dp[i]=Math.max(nums[i]+dp[i+2],dp[i+1]);
         }
-        return nums[0];
+        return Math.max(dp[0],dp[1]);
     }
 }
