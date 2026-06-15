dd27ac02de0a879377ae29d33a5d9f7368f37c42
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-06-15 23:40:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.java
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.java
@@ -1,26 +1,24 @@
 class Solution {
     int dp[][];
-    public int helper(int val[], int wt[], int c,int n)
+    public int helper(int val[],int wt[],int capacity, int i)
     {
-        
-        if(c==0){return 0;}
-        if(c<0||n<0){return 0;}
-        //System.out.print(wt[n]+" "+c);
-        if(dp[n][c]!=-1){return dp[n][c];}
-        if(wt[n]<=c)
+        if(capacity==0||i<0){return 0;}
+        //System.out.print(capacity+" ");
+        if(dp[i][capacity]!=-1){return dp[i][capacity];}
+        if(wt[i]<=capacity)
         {
-            
-            return dp[n][c]=Math.max(val[n]+helper(val,wt,c-wt[n],n),helper(val,wt,c,n-1));
+            return dp[i][capacity]=Math.max(helper(val,wt,capacity-wt[i],i)+val[i],helper(val,wt,capacity,i-1));    
         }
-        return dp[n][c]=helper(val,wt,c,n-1);
+        return dp[i][capacity]=helper(val,wt,capacity,i-1);
+        
     }
     public int knapSack(int val[], int wt[], int capacity) {
         // code here
-        dp=new int[wt.length+1][capacity+1];
-        for(int i=0;i<wt.length+1;++i)
+        dp=new int[val.length+1][capacity+1];
+        for(int i=0;i<val.length+1;++i)
         {
             Arrays.fill(dp[i],-1);
         }
-        return helper(val,wt,capacity,wt.length-1);
+        return helper(val,wt,capacity,val.length-1);
     }
 }
