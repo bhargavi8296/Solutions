66802144f67dc39252a84dfb6284ff75f52a9c85
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-06-15 14:59:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -1,22 +1,32 @@
 class Solution {
     static int dp[][];
-    static boolean helper(int arr[], int sum, int ind)
+    static boolean helper(int arr[], int sum ,int i)
     {
         if(sum==0){return true;}
-        if(sum<0||ind<0){return false;}
-        if(dp[ind][sum]!=-1){return dp[ind][sum]==1?true:false;}
-        boolean flag=helper(arr,sum-arr[ind],ind-1)|helper(arr,sum,ind-1);
-        dp[ind][sum]=flag==true?1:0;
+        if(i<0){return false;}
+        if(dp[i][sum]!=-1)
+        {
+            return dp[i][sum]==1?true:false;
+        }
+        boolean flag;
+        if(sum>=arr[i])
+        {
+            flag= helper(arr,sum-arr[i],i-1)|helper(arr,sum,i-1);
+            dp[i][sum]=flag==true?1:0;
+            return flag;
+        }
+        flag= helper(arr,sum, i-1);
+        dp[i][sum]=flag==true?1:0;
         return flag;
     }
     static Boolean isSubsetSum(int arr[], int sum) {
         // code here
         dp=new int[arr.length+1][sum+1];
-        for(int i=0;i<arr.length+1;++i)
+        for(int i=0;i<arr.length;++i)
         {
             Arrays.fill(dp[i],-1);
         }
-        return helper(arr,sum,arr.length-1);
+        return helper(arr,sum, arr.length-1);
         
     }
 }
