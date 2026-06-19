53916b828c13aadf82272bf6975a22d0f26a05a2
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-06-19 14:36:07 +0530

    Time: 20 ms (64.11%) | Memory: 62.8 MB (21.61%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,28 +1,28 @@
 class Solution {
     int dp[][];
-    public boolean helper(String s, String p, int i, int j)
+    public boolean helper(String s, String t, int i, int j)
     {
-        if(i==0&&j==0){return true;}
-        else if(i>0&&j==0){return false;}
-        else if(i==0&&j>0){if(p.charAt(j-1)=='*'){return helper(s,p,i,j-1);}return false;}
+        if(i<0&&j<0){return true;}
+        if(i<0){if(t.charAt(j)=='*'){return helper(s,t,i,j-1);}return false;}
+        if(j<0){return false;}
         if(dp[i][j]!=-1){return dp[i][j]==1?true:false;}
         boolean flag;
-        if(p.charAt(j-1)=='?'){
-            flag=helper(s,p,i-1,j-1);
-            dp[i][j]=flag==true?1:0;
-            return flag;
-            }
-        else if(p.charAt(j-1)>='a'&&p.charAt(j-1)<='z')
+        if(t.charAt(j)=='?')
         {
-            if(p.charAt(j-1)!=s.charAt(i-1)){flag= false;}
-            else{flag=helper(s,p,i-1,j-1);}
-            dp[i][j]=flag==true?1:0;
-            return flag;
+            flag= helper(s,t,i-1,j-1);
         }
-        flag=helper(s,p,i-1,j)||helper(s,p,i,j-1);
+        else if(t.charAt(j)=='*')
+        {
+            flag= helper(s,t,i-1,j-1)||helper(s,t,i,j-1)||helper(s,t,i-1,j);
+        }
+        else if(s.charAt(i)!=t.charAt(j))
+        {
+            flag= false;
+        }
+        else
+        {flag= helper(s,t,i-1,j-1);}
         dp[i][j]=flag==true?1:0;
         return flag;
-
     }
     public boolean isMatch(String s, String p) {
         dp=new int[s.length()+1][p.length()+1];
@@ -30,6 +30,6 @@ class Solution {
         {
             Arrays.fill(dp[i],-1);
         }
-            return helper(s,p,s.length(),p.length());
+        return helper(s,p,s.length()-1,p.length()-1);
     }
 }
