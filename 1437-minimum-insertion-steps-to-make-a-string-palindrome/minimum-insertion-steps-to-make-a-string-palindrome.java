224610780fec71bab01053e8bbe512ee548bdb00
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-06-19 13:24:18 +0530

    Time: 25 ms (63.00%) | Memory: 47.8 MB (10.76%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,22 +1,22 @@
 class Solution {
     int dp[][];
-    public int helper(String s1,String s2, int i, int j)
+    public int helper(String s, int i, int j)
     {
-        if(i<0||j<0){return 0;}
-        if(dp[i][j]!=-1){return dp[i][j];}
-        if(s1.charAt(i)==s2.charAt(j))
+        if(i>j){return 0;}
+        if(i>=s.length()||j<0){return 501;}
+        if(dp[i][j]!=501){return dp[i][j];}
+        if(s.charAt(i)==s.charAt(j))
         {
-            return dp[i][j]=1+helper(s1,s2,i-1,j-1);
+            return dp[i][j]=helper(s,i+1,j-1);
         }
-        return dp[i][j]=Math.max(helper(s1,s2,i-1,j),helper(s1,s2,i,j-1));
+        return dp[i][j]=Math.min(helper(s,i+1,j-1)+2,Math.min(helper(s,i+1,j),helper(s,i,j-1))+1);
     }
     public int minInsertions(String s) {
         dp=new int[s.length()+1][s.length()+1];
         for(int i=0;i<s.length()+1;++i)
         {
-            Arrays.fill(dp[i],-1);
+            Arrays.fill(dp[i],501);
         }
-        int res=helper(s,new StringBuilder(s).reverse().toString(),s.length()-1,s.length()-1);
-        return s.length()-res;
+        return helper(s,0,s.length()-1);
     }
 }
