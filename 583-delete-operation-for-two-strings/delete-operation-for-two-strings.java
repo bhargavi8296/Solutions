b2b6707948414f935d996533e855f1160642f314
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-06-19 13:31:49 +0530

    Time: 7 ms (68.80%) | Memory: 46.7 MB (13.43%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -1,21 +1,22 @@
 class Solution {
     int dp[][];
-    public int helper(String s1, String s2, int i, int j)
+    public int helper(String s, String t, int i, int j)
     {
-        if(i<0||j<0){return 0;}
-        if(dp[i][j]!=-1){return dp[i][j];}
-        if(s1.charAt(i)==s2.charAt(j))
+        if(i<0&&j<0){return 0;}
+        if(i<0||j<0){return i<0?j+1:i+1;}
+        if(dp[i][j]!=501){return dp[i][j];}
+        if(s.charAt(i)==t.charAt(j))
         {
-            return dp[i][j]=1+helper(s1,s2,i-1,j-1);
+            return dp[i][j]=helper(s,t,i-1,j-1);
         }
-        return dp[i][j]=Math.max(helper(s1,s2,i-1,j),helper(s1,s2,i,j-1));
+        return dp[i][j]=Math.min(helper(s,t,i-1,j),helper(s,t,i,j-1))+1;
     }
     public int minDistance(String word1, String word2) {
         dp=new int[word1.length()+1][word2.length()+1];
         for(int i=0;i<word1.length()+1;++i)
         {
-            Arrays.fill(dp[i],-1);
+            Arrays.fill(dp[i],501);
         }
-        return word1.length()+word2.length()-2*(helper(word1,word2,word1.length()-1,word2.length()-1));
+        return helper(word1,word2,word1.length()-1,word2.length()-1);
     }
 }
