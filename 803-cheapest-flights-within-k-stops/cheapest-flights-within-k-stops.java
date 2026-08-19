1106481863bf10d460cf33d3dda247d92a271115
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-08-19 14:01:09 +0530

    Time: 6 ms (85.48%) | Memory: 46 MB (98.98%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,38 +1,58 @@
+class pair{
+    int node;
+    int dist;
+    int k;
+    pair(int node, int dist, int k)
+    {
+        this.node=node;
+        this.dist=dist;
+        this.k=k;
+    }
+}
+class Pair{
+    int node;
+    int dist;
+    Pair(int node, int dist)
+    {
+        this.node=node;
+        this.dist=dist;
+    }
+}
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        List<List<int[]>>list=new ArrayList<>();
-        for(int i=0;i<n;++i){list.add(new ArrayList<>());}
+        int ds[]=new int[n];
+        List<List<Pair>>list=new ArrayList<>();
+        for(int i=0;i<n;++i)
+        {
+            list.add(new ArrayList<>());
+        }
         for(int i=0;i<flights.length;++i)
         {
             int f=flights[i][0];
             int s=flights[i][1];
             int d=flights[i][2];
-            list.get(f).add(new int[]{s,d});
+            list.get(f).add(new Pair(s,d));
         }
-        Queue<int[]>q=new LinkedList<>();
-        q.add(new int[]{src,0,k});
-        int dist[]=new int[n];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-        dist[src]=0;
+        Arrays.fill(ds, Integer.MAX_VALUE);
+        ds[src]=0;
+        Queue<pair>q=new LinkedList<>();
+        q.add(new pair(src,0,k));
         while(!q.isEmpty())
         {
-            int []temp=q.poll();
-            int sr=temp[0];
-            int d=temp[1];
-            int c=temp[2];
-            //System.out.println(sr+" "+d+" "+c);
-            //if(sr==dst){return d;}
-            if(c<0){continue;}
-            for(int t[]:list.get(sr))
+            pair temp=q.poll();
+            int node=temp.node;
+            int dis=temp.dist;
+            int K=temp.k;
+            if(K<0){continue;}
+            for(Pair t:list.get(node))
             {
-                int val=t[0];int di=t[1];
-               if(d + di < dist[val])
+                if(ds[t.node]>dis+t.dist)
                 {
-                    dist[val] = d + di;
-                    q.add(new int[]{val,dist[val],c-1});
+                    ds[t.node]=dis+t.dist;
+                    q.add(new pair(t.node,ds[t.node],K-1));
                 }
             }
         }
-        return dist[dst]==Integer.MAX_VALUE?-1:dist[dst];
+        return ds[dst]==Integer.MAX_VALUE?-1:ds[dst];
     }
 }
