87e4154ce08fd4c526a7978ece2c6b8808b7b273
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-08-19 07:42:40 +0530

    Time: 10 ms (80.43%) | Memory: 49.1 MB (45.73%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,32 +1,44 @@
+class pair{
+    int node;
+    int dist;
+    pair(int node, int dist)
+    {
+        this.node=node;
+        this.dist=dist;
+    }
+}
 class Solution {
+    int dist[];
+    ArrayList<ArrayList<pair>>list;
     public int networkDelayTime(int[][] times, int n, int k) {
-        int dist[]=new int[n+1];
-        ArrayList<ArrayList<int[]>>list=new ArrayList<>();
-        for(int i=0;i<=n;++i){list.add(new ArrayList<>());}
+        dist=new int[n+1];
+        list=new ArrayList<>();
+        for(int i=0;i<=n;++i)
+        {
+            list.add(new ArrayList<>());
+        }
         for(int i=0;i<times.length;++i)
         {
             int f=times[i][0];
             int s=times[i][1];
             int d=times[i][2];
-            list.get(f).add(new int[]{s,d});
+            list.get(f).add(new pair(s,d));
         }
-        Queue<int[]>q=new PriorityQueue<>((a,b)->a[1]-b[1]);
         Arrays.fill(dist,Integer.MAX_VALUE);
         dist[k]=0;
-        q.add(new int[]{k,0});
+        Queue<pair>q=new PriorityQueue<>((a,b)->a.dist-b.dist);
+        q.add(new pair(k,0));
         while(!q.isEmpty())
         {
-            int[]p=q.poll();
-            int f=p[0];
-            int d=p[1];
-            for(int []t:list.get(f))
+            pair temp=q.poll();
+            int node=temp.node;
+            int dis=temp.dist;
+            for(pair p:list.get(node))
             {
-                int val=t[0];
-                int di=t[1];
-                if(dist[val]>d+di)
+                if(dist[p.node]>dis+p.dist)
                 {
-                    dist[val]=d+di;
-                    q.add(new int[]{val,dist[val]});
+                    dist[p.node]=dis+p.dist;
+                    q.add(new pair(p.node,dist[p.node]));
                 }
             }
         }
@@ -34,7 +46,9 @@ class Solution {
         for(int i=1;i<=n;++i)
         {
             if(dist[i]==Integer.MAX_VALUE){return -1;}
-            else{res=Math.max(res,dist[i]);}
+            else{
+                res=Math.max(res,dist[i]);
+            }
         }
         return res;
     }
