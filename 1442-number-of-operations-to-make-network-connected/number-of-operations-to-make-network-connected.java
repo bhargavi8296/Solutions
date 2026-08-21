42dfa5f01c38db35b0c1b8b68d934f2a60e5a9ed
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-08-21 14:29:45 +0530

    Time: 5 ms (76.03%) | Memory: 65.4 MB (89.29%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -1,29 +1,37 @@
 class Solution {
     int par[];
-    int rank[];
+    int range[];
+    int count[];
     public int find(int x)
     {
         if(x==par[x]){return x;}
         return par[x]=find(par[x]);
     }
-    public void union(int px, int py)
+    public void union(int x, int y)
     {
-        if(rank[px]>rank[py])
+        if(range[x]>range[y])
         {
-            par[py]=px;
-            rank[px]+=rank[py];
+            range[x]+=range[y];
+            par[y]=x;
+            count[x]+=1;
+            count[x]+=count[y];
         }
-        else
-        {
-            par[px]=py;
-            rank[py]+=rank[px];
+        else{
+            range[y]+=range[x];
+            par[x]=y;
+            count[y]+=1;
+            count[y]+=count[x];
         }
     }
     public int makeConnected(int n, int[][] connections) {
-        par=new int[n];rank=new int[n];
-        Arrays.fill(rank,1);
-        for(int i=0;i<n;++i){par[i]=i;}
-        int count=0;
+        par=new int[n];
+        range=new int[n];
+        count=new int[n];
+        for(int i=0;i<n;++i)
+        {
+            par[i]=i;
+        }
+        Arrays.fill(range,1);
         for(int i=0;i<connections.length;++i)
         {
             int x=connections[i][0];
@@ -34,14 +42,20 @@ class Solution {
             {
                 union(px,py);
             }
-            else{++count;}
+            else{
+                count[px]++;
+            }
         }
         int c=0;
+        int t=0;
         for(int i=0;i<n;++i)
         {
-            if(par[i]==i){++c;}
+            if(par[i]==i)
+            {
+                ++c;
+                t+=(count[i]-(range[i]-1));
+            }
         }
-        //System.out.print(count+" "+c);
-        return count>=(c-1)?c-1:-1;
+        return t>=c-1?c-1:-1;
     }
 }
