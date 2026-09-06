ef7dab146c7604607ce47930f79ae844b4b6bc35
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-09-06 18:07:51 +0530

    Time: 8 ms (60.60%) | Memory: 42.8 MB (84.19%) - LeetSync

---
+++ 38-count-and-say/count-and-say.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String countAndSay(int n) {
+        String str="1";
+        if(n==1){return str;}
+        --n;
+        while(n>0)
+        {
+            //System.out.println(str);
+            int i=0;
+            int j=i;
+            int count=0;
+            StringBuilder temp=new StringBuilder();
+            while(j<str.length())
+            {
+                count=0;
+                while(j<str.length()&&str.charAt(j)==str.charAt(i))
+                {
+                    ++count;++j;
+                }
+                temp.append(count).append(str.charAt(i));
+                i=j;
+                //++j;
+            }
+            if(i<str.length()){
+                temp.append(count).append(str.charAt(i));
+            }
+            str=temp.toString();
+            --n;
+        }
+        return str;
+    }
+}
