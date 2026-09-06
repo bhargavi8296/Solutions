493226710e9804532413f5d69656336fecd26863
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-09-06 17:32:19 +0530

    Time: 12 ms (5.93%) | Memory: 46.2 MB (47.08%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,33 +1,32 @@
 class Solution {
-    List<String>wD;
     int temp[];
-    public boolean helper(String s, int i)
+    public boolean helper(String s, List<String>w, int i)
     {
         if(i==s.length()){return true;}
-        String str="";
-        if(temp[i]!=-1)
-        {
-            return temp[1]==1?true:false;
+        if(temp[i]!=-1){
+            return temp[i]==1?true:false;
         }
+        String str="";
         for(int j=i;j<s.length();++j)
         {
             str+=s.charAt(j);
-            if(wD.contains(str))
+            //w.remove(str);
+            if(w.contains(str))
             {
-                if(helper(s,j+1)){
+                if(helper(s,w,j+1)){
                     temp[i]=1;
                     return true;}
                 else{
                     temp[i]=0;
                 }
             }
+            //w.add(str);
         }
         return false;
     }
     public boolean wordBreak(String s, List<String> wordDict) {
-        wD=wordDict;
         temp=new int[s.length()];
         Arrays.fill(temp,-1);
-        return helper(s,0);
+        return helper(s,wordDict,0);
     }
 }
