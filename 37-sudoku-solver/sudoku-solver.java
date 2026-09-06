d23d2fa32e3fee6b7410d515b0068a29c46c46a8
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-09-06 17:12:46 +0530

    Time: 182 ms (79.25%) | Memory: 42.7 MB (88.33%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,44 +1,41 @@
 class Solution {
-    char b[][];
-    public boolean check(int r,int c, char num)
+    public boolean verify(char[][]board, int r, int c, char ch)
     {
+        for(int i=0;i<9;++i)
+        {
+            if(board[i][c]==ch||board[r][i]==ch){return false;}
+        }
         int sr=(r/3)*3;
         int sc=(c/3)*3;
-        for(int i=0;i<9;++i){if(b[r][i]==num||b[i][c]==num){return false;}}
         for(int i=sr;i<sr+3;++i)
         {
             for(int j=sc;j<sc+3;++j)
             {
-                if(b[i][j]==num){return false;}
+                if(board[i][j]==ch){return false;}
             }
         }
         return true;
     }
-    public boolean helper(int r, int c)
-    {
-        if(r==9){return true;}
-        int tr=r;
-        int tc=c;
-        if(c+1==9){tr=r+1;tc=0;}
-        else{tc=c+1;}
-        //System.out.println(tr+" "+tc);
-        if(b[r][c]=='.')
+    public boolean helper(char[][] board,int r, int c){
+        if(r==board.length){return true;}
+        if(board[r][c]!='.'){
+            int tc=c+1==9?0:c+1;
+            int tr=c+1==9?r+1:r;
+            return helper(board,tr,tc);
+        } 
+        for(int i=0;i<9;++i)
         {
-            for(int i=1;i<=9;++i)
-            {
-                if(check(r,c,(char)('0' + i)))
-                {
-                    b[r][c]=(char)('0' + i);
-                    if(helper(tr,tc)){return true;}
-                    b[r][c]='.';
-                }
+            if(verify(board,r,c,(char)('0'+i+1))){
+                board[r][c]=(char)('0'+i+1);
+                int tc=c+1==9?0:c+1;
+                int tr=c+1==9?r+1:r;
+                if(helper(board,tr,tc)){return true;}
+                board[r][c]='.';
             }
-            return false;
         }
-        return helper(tr,tc);
+        return false;
     }
     public void solveSudoku(char[][] board) {
-        b=board;
-        helper(0,0);
+           helper(board,0,0);
     }
 }
