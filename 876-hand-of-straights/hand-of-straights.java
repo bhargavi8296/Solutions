d0da86392db943d12db7b092db3a1286542ad15f
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-09-10 22:34:13 +0530

    Time: 500 ms (5.12%) | Memory: 47.4 MB (96.96%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -1,48 +1,71 @@
 class pair{
-    int num;
-    int val;
-    pair(int num, int val)
+    int num;int val;
+    pair(int num,int val)
     {
         this.num=num;
         this.val=val;
-    }
+    } 
 }
 class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        Queue<pair>q=new PriorityQueue<>((a,b)->a.num-b.num);
         Map<Integer,Integer>map=new HashMap<>();
-        if(((hand.length)%groupSize)!=0){return false;}
-        if(groupSize==1){return true;}
-        for(int val:hand){map.put(val,map.getOrDefault(val,0)+1);}
-        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
-            q.add(new pair(e.getKey(),e.getValue()));
-        }
-        int total=hand.length;
-        while(!q.isEmpty())
+        for(int i=0;i<hand.length;++i)
         {
-            pair top=q.poll();
-            int num=top.num;
-            int val=top.val;
-            total--;
-            int size=Math.min(groupSize-1,q.size());
-            if(size<groupSize-1&&size<total){return false;}
-            Queue<pair>q1=new PriorityQueue<>((a,b)->a.num-b.num);
-            if(val>1){q1.add(new pair(num,val-1));}
-            while(size>0)
+            map.put(hand[i],map.getOrDefault(hand[i],0)+1);
+        }
+        PriorityQueue<pair>p1=new PriorityQueue<>((a,b)->a.num-b.num);
+        PriorityQueue<pair>p2=new PriorityQueue<>((a,b)->a.num-b.num);
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            p1.add(new pair(entry.getKey(),entry.getValue()));
+        }
+        while(!p1.isEmpty()||!p2.isEmpty())
+        {
+            int size=groupSize;
+            if(!p1.isEmpty())
             {
-                pair temp=q.poll();
-                total--;
-                int num1=temp.num;
-                int val1=temp.val;
-                //System.out.println(num+" "+num1+" "+val+" "+val1);
-                if(num1-num!=1){return false;}
-                else{
-                    if(val1>1){q1.add(new pair(num1,val1-1));}
-                    num=num1;
+                int res=p1.peek().num-1;
+                while(size>0&&!p1.isEmpty())
+                {
+                    pair temp=p1.poll();
+                    if(temp.num-1!=res){return false;}
+                    res=temp.num;
+                    temp.val--;
+                    if(temp.val>0)
+                    {
+                        //System.out.println(temp.num+" "+temp.val);
+                        p2.add(temp);
+                    }
+                    --size;
                 }
-                --size;
+                //System.out.println(size);
+                if(size>0){return false;}
+                p2.addAll(p1);
+                p1.clear();
+                //while(!p1.isEmpty()){p2.add(p1.poll());}
+                //System.out.println(p2);
             }
-            q.addAll(q1);
+            else if(!p2.isEmpty())
+            {
+                
+                int res=p2.peek().num-1;
+                while(size>0&&!p2.isEmpty())
+                {
+                    pair temp=p2.poll();
+                    if(temp.num-1!=res){return false;}
+                    res=temp.num;
+                    temp.val--;
+                    if(temp.val>0)
+                    {
+                        p1.add(temp);
+                    }
+                    --size;
+                }
+
+                if(size>0){return false;}
+                p1.addAll(p2);
+                p2.clear();
+                //while(!p2.isEmpty()){p1.add(p2.poll());}
+            }
         }
         return true;
     }
