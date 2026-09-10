ca426180fc803793e84b4a6e6e588267970d03ec
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-09-10 22:36:22 +0530

    Time: 32 ms (69.49%) | Memory: 47.2 MB (98.80%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -18,11 +18,9 @@ class Solution {
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             p1.add(new pair(entry.getKey(),entry.getValue()));
         }
-        while(!p1.isEmpty()||!p2.isEmpty())
+        while(!p1.isEmpty())
         {
             int size=groupSize;
-            if(!p1.isEmpty())
-            {
                 int res=p1.peek().num-1;
                 while(size>0&&!p1.isEmpty())
                 {
@@ -37,35 +35,10 @@ class Solution {
                     }
                     --size;
                 }
-                //System.out.println(size);
-                if(size>0){return false;}
-                p2.addAll(p1);
-                p1.clear();
-                //while(!p1.isEmpty()){p2.add(p1.poll());}
-                //System.out.println(p2);
-            }
-            else if(!p2.isEmpty())
-            {
-                
-                int res=p2.peek().num-1;
-                while(size>0&&!p2.isEmpty())
-                {
-                    pair temp=p2.poll();
-                    if(temp.num-1!=res){return false;}
-                    res=temp.num;
-                    temp.val--;
-                    if(temp.val>0)
-                    {
-                        p1.add(temp);
-                    }
-                    --size;
-                }
-
                 if(size>0){return false;}
                 p1.addAll(p2);
                 p2.clear();
-                //while(!p2.isEmpty()){p1.add(p2.poll());}
-            }
+            
         }
         return true;
     }
