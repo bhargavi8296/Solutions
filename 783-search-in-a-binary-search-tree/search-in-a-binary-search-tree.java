f10da1420adba5b45955ccb2745d773e76b631a6
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-09-16 14:21:39 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (74.28%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
@@ -17,8 +17,7 @@ class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
         if(root==null){return null;}
         if(root.val==val){return root;}
-        TreeNode left= searchBST(root.left,val);
-        TreeNode right=searchBST(root.right,val);
-        return left!=null?left:right;
+        else if(root.val>val){return searchBST(root.left,val);}
+        return searchBST(root.right,val);
     }
 }
