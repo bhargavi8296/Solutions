4bcd0b16b46941501436559bc65a500e5ffe9828
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-09-19 15:31:44 +0530

    Time: 21 ms (23.87%) | Memory: 82.5 MB (82.96%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,32 +1,40 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
         if((m*k)>bloomDay.length){return -1;}
-        int min=Integer.MAX_VALUE;
-        int max=Integer.MIN_VALUE;
+        int l=Integer.MAX_VALUE;
+        int h=Integer.MIN_VALUE;
         for(int i=0;i<bloomDay.length;++i)
         {
-            min=Math.min(min,bloomDay[i]);
-            max=Math.max(max,bloomDay[i]);
+            l=Math.min(l,bloomDay[i]);
+            h=Math.max(h,bloomDay[i]);
         }
-        int result=-1;
-        while(min<=max)
+        int ans=-1;
+        while(l<=h)
         {
-            int mid=(min+max)/2;
-            //System.out.println(mid+" ");
-            int i=0;
-            int res=0;
-            while(i<bloomDay.length)
+            int mid=(l+h)/2;
+            int time=0;
+            int count=0;
+            for(int i=0;i<bloomDay.length;++i)
             {
-                int count=0;
-                while(i<bloomDay.length&&bloomDay[i]<=mid)
+                if(bloomDay[i]<=mid)
                 {
-                    ++count;++i;
+                    ++count;
                 }
-                res+=(count/k);++i;
+                else{
+                    time+=(count/k);
+                    count=0;
+                }
             }
-            if(res>=m){result=mid;max=mid-1;}
-            else{min=mid+1;}
+            time+=(count/k);
+            //System.out.println(time+" "+mid);
+            if(time>=m){
+                ans=mid;
+                h=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
         }
-        return result;
+        return ans;
     }
 }
