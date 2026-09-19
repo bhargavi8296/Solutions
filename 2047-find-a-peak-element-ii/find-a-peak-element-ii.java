b822c74e79c35f87557aca3de43bee531bb973a4
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-09-19 17:02:49 +0530

    Time: 0 ms (100.00%) | Memory: 118.9 MB (88.99%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
@@ -1,28 +1,31 @@
 class Solution {
-    public int max(int[][]mat,int col){
-    int row=-1;
-    int val=Integer.MIN_VALUE;
-    for(int i=0;i<mat.length;++i)
-    {
-        if(val<mat[i][col]){val=mat[i][col];row=i;}
-    }
-    return row;
-}
     public int[] findPeakGrid(int[][] mat) {
-        int low=0;
-        int high=mat[0].length-1;
-        while(low<=high)
+        int l=0;
+        int h=mat[0].length-1;
+        int result[]=new int[2];
+        while(l<=h)
         {
-            int mid=(low+high)/2;
-            int row=max(mat,mid);
-            int left=mid-1>=0?mat[row][mid-1]:Integer.MIN_VALUE;
-            int right=mid+1<mat[0].length?mat[row][mid+1]:Integer.MIN_VALUE;
-            int val=mat[row][mid];
-            //System.out.println(left+" "+right+" "+val);
-            if(val>left&&val>right){return new int[]{row, mid};}
-            if(val>left){low=mid+1;}
-            if(val>right){high=mid-1;}
+            int mid=(l+h)/2;
+            int ind=0;
+            for(int i=0;i<mat.length;++i)
+            {
+                if(mat[ind][mid]<mat[i][mid])
+                {
+                    ind=i;
+                }
+            }
+            int left=mid-1<0?Integer.MIN_VALUE:mat[ind][mid-1];
+            int right=mid+1>=mat[0].length?Integer.MIN_VALUE:mat[ind][mid+1];
+            if(mat[ind][mid]>left&&mat[ind][mid]>right){
+                result[0]=ind;
+                result[1]=mid;
+                break;
+            }
+            else if(mat[ind][mid]>left){l=mid+1;}
+            else{
+                h=mid-1;
+            }
         }
-        return new int[2];
+        return result;
     }
 }
