93de7f67cb8aca42e10f995a82009239946bb0d2
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-09-20 09:19:04 +0530

    Time: 8 ms (71.77%) | Memory: 58.1 MB (64.13%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,8 +1,6 @@
 class Solution {
-    public void helper(int[][]grid,int r,int c)
+    public void helper(int[][]grid, int r, int c)
     {
-        if(grid[r][c]==0){return;}
-        grid[r][c]=0;
         int ro[]={0,1,0,-1};
         int co[]={1,0,-1,0};
         for(int i=0;i<4;++i)
@@ -11,20 +9,23 @@ class Solution {
             int tc=c+co[i];
             if(tr>=0&&tr<grid.length&&tc>=0&&tc<grid[0].length&&grid[tr][tc]==1)
             {
+                grid[tr][tc]=0;
                 helper(grid,tr,tc);
             }
         }
+
     }
     public int numEnclaves(int[][] grid) {
         for(int i=0;i<grid.length;++i)
         {
-            for(int j=0;j<grid[i].length;++j)
-            {
-                if((i==0||j==0||i==grid.length-1||j==grid[0].length-1)&&grid[i][j]==1)
-                {
-                    helper(grid,i,j);
-                }
-            }
+            //System.out.println(grid[i][0]+" "+i+" ");
+            if(grid[i][0]==1){grid[i][0]=0;helper(grid,i,0);}
+            if(grid[i][grid[0].length-1]==1){grid[i][grid[0].length-1]=0;helper(grid,i,grid[0].length-1);}
+        }
+        for(int i=0;i<grid[0].length;++i)
+        {
+            if(grid[0][i]==1){grid[0][i]=0;helper(grid,0,i);}
+            if(grid[grid.length-1][i]==1){grid[grid.length-1][i]=0;helper(grid,grid.length-1,i);}
         }
         int count=0;
         for(int i=0;i<grid.length;++i)
