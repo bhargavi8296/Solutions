cfd5700f14dbe3c19acd46b79c4158131ef51264
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-09-20 10:27:21 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (94.01%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,27 +1,26 @@
 class Solution {
     int color[];
-    int visited[];
     public boolean helper(int[][]graph,int i)
     {
+        //System.out.println(i);
         for(int val:graph[i])
         {
-            if(visited[val]==0){visited[val]=1;color[val]=color[i]==0?1:0;if(!helper(graph,val)){return false;}}
-            else{if(color[i]==color[val]){return false;}}
+            if(color[val]!=-1)
+            {
+                if(color[val]==color[i]){return false;}
+                continue;
+            }
+            color[val]=color[i]==1?0:1;
+            if(!helper(graph,val)){return false;}
         }
         return true;
     }
     public boolean isBipartite(int[][] graph) {
         color=new int[graph.length];
-        visited=new int[graph.length];
         Arrays.fill(color,-1);
         for(int i=0;i<graph.length;++i)
         {
-            if(color[i]==-1)
-            {
-                color[i]=0;
-                visited[i]=1;
-                if(!helper(graph,i)){return false;}
-            }
+            if(color[i]==-1){color[i]=0;if(!helper(graph,i)){return false;}}
         }
         return true;
     }
