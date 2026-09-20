c25a034116bf031b613da65a741cc39e1f93cfe8
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-09-20 10:15:21 +0530

    Time: 4 ms (45.70%) | Memory: 50.9 MB (98.50%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,8 +1,6 @@
 class Solution {
-    public void helper(char[][]grid,int r, int c)
+    public void helper(char[][]grid,int r,int c)
     {
-        if(grid[r][c]=='0'){return;}
-        grid[r][c]='0';
         int ro[]={0,1,0,-1};
         int co[]={1,0,-1,0};
         for(int i=0;i<4;++i)
@@ -11,6 +9,7 @@ class Solution {
             int tc=c+co[i];
             if(tr>=0&&tr<grid.length&&tc>=0&&tc<grid[0].length&&grid[tr][tc]=='1')
             {
+                grid[tr][tc]='0';
                 helper(grid,tr,tc);
             }
         }
@@ -23,8 +22,9 @@ class Solution {
             {
                 if(grid[i][j]=='1')
                 {
-                    ++count;
+                    grid[i][j]='0';
                     helper(grid,i,j);
+                    ++count;
                 }
             }
         }
