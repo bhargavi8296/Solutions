53b79252fdf97d0fa34c59ca39454cda6a11531e
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-09-20 11:15:10 +0530

    Time: 1492 ms (5.15%) | Memory: 50.6 MB (5.19%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,45 +1,48 @@
-class pair{
-    int r;
-    int c;
-    pair(int r,int c)
-    {
-        this.r=r;
-        this.c=c;
-    }
-}
 class Solution {
+    int dist[][];
+    public void helper(int [][]mat,int r,int c)
+    {
+        int ro[]={0,1,0,-1};
+        int co[]={1,0,-1,0};
+        for(int i=0;i<4;++i)
+        {
+            int tr=r+ro[i];
+            int tc=c+co[i];
+            if(tr>=0&&tr<mat.length&&tc>=0&&tc<mat[0].length&&mat[tr][tc]==1&&dist[tr][tc]>dist[r][c]+1)
+            {
+                dist[tr][tc]=dist[r][c]+1;
+                helper(mat,tr,tc);
+            }
+        }
+    }
     public int[][] updateMatrix(int[][] mat) {
-        int res[][]=new int[mat.length][mat[0].length];
-        for(int i=0;i<mat.length;++i){Arrays.fill(res[i],Integer.MAX_VALUE);}
-        Queue<pair>q=new LinkedList<>();
+        dist=new int[mat.length][mat[0].length];
+        for(int i=0;i<mat.length;++i)
+        {
+            Arrays.fill(dist[i],100000);
+        }
         for(int i=0;i<mat.length;++i)
         {
             for(int j=0;j<mat[i].length;++j)
             {
                 if(mat[i][j]==0)
                 {
-                    q.add(new pair(i,j));
-                    res[i][j]=0;
-                    
+                    dist[i][j]=0;
+                    helper(mat,i,j);
                 }
             }
         }
-        while(!q.isEmpty())
-        {
-            pair p=q.poll();
-            int r=p.r;int c=p.c;
-            int ro[]={1,0,-1,0};
-            int co[]={0,1,0,-1};
-            for(int i=0;i<4;++i)
-            {
-                int tr=r+ro[i];int tc=c+co[i];
-                if(tr>=0&&tr<mat.length&&tc>=0&&tc<mat[0].length&&mat[tr][tc]==1&&res[tr][tc]>res[r][c]+1)
-                {
-                    res[tr][tc]=res[r][c]+1;
-                    q.add(new pair(tr,tc));
-                }
-            }
-        }
-        return res;
+        // for(int i=0;i<mat.length;++i)
+        // {
+        //     for(int j=0;j<mat[i].length;++j)
+        //     {
+        //         if(dist[i][j]>10001)
+        //         {
+        //             dist[i][j]=0;
+        //             helper(mat,i,j);
+        //         }
+        //     }
+        // }
+        return dist;
     }
 }
