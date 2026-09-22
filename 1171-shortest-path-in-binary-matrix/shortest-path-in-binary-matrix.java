936bd403e92a6ecac08c0ef5532ee54ddbea6545
bhargavi8296 <68277305+bhargavi8296@users.noreply.github.com>
2026-09-22 14:02:04 +0530

    Time: 32 ms (11.34%) | Memory: 47.8 MB (47.56%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -2,7 +2,7 @@ class pair{
     int r;
     int c;
     int d;
-    pair(int r,int c,int d)
+    pair(int r,int c, int d)
     {
         this.r=r;
         this.c=c;
@@ -10,35 +10,34 @@ class pair{
     }
 }
 class Solution {
-    int dist[][];
     public int shortestPathBinaryMatrix(int[][] grid) {
-        dist=new int[grid.length][grid[0].length];
+        int dist[][]=new int[grid.length][grid.length];
         for(int i=0;i<grid.length;++i)
         {
             Arrays.fill(dist[i],Integer.MAX_VALUE);
         }
-        Queue<pair>q=new LinkedList<>();
-        if(grid[0][0]==0){dist[0][0]=1;q.add(new pair(0,0,1));}
+        if(grid[0][0]==1||grid[grid.length-1][grid.length-1]==1){return -1;}
+        Queue<pair>q=new PriorityQueue<>((a,b)->a.d-b.d);
+        dist[0][0]=1;
+        q.add(new pair(0,0,1));
+        int ro[]={0,1,0,-1,1,1,-1,-1};
+        int co[]={1,0,-1,0,1,-1,-1,1};
         while(!q.isEmpty())
         {
-            pair p=q.poll();
-            int r=p.r;
-            int c=p.c;
-            int d=p.d;
-            int ro[]={0,1,0,-1,1,1,-1,-1};
-            int co[]={1,0,-1,0,1,-1,1,-1};
+            pair temp=q.poll();
+            int r=temp.r;
+            int c=temp.c;
             for(int i=0;i<8;++i)
             {
-                int tr=ro[i]+r;
-                int tc=co[i]+c;
-                if(tr>=0&&tr<grid.length&&tc>=0&&tc<grid[0].length&&grid[tr][tc]==0&&dist[tr][tc]>d+1)
+                int tr=r+ro[i];
+                int tc=c+co[i];
+                if(tr>=0&&tr<grid.length&&tc>=0&&tc<grid.length&&grid[tr][tc]==0&&dist[tr][tc]>dist[r][c]+1)
                 {
-                    dist[tr][tc]=d+1;
+                    dist[tr][tc]=dist[r][c]+1;
                     q.add(new pair(tr,tc,dist[tr][tc]));
                 }
             }
         }
-        int res=dist[grid.length-1][grid[0].length-1];
-        return res==Integer.MAX_VALUE?-1:res;
+        return dist[grid.length-1][grid.length-1]==Integer.MAX_VALUE?-1:dist[grid.length-1][grid.length-1];
     }
 }
